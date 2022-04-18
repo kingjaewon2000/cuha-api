5b03bd79ee32d2867fb885fc0bf7f047a2018975
kingjaewon2000 <kkkxx_00@naver.com>
2022-04-19 03:50:40 +0900

    2022-04-19 (화) querydsl 적용

---
+++ src/test/java/com/cju/cuhaapi/QueryDslTest.java
@@ -0,0 +1,62 @@
+package com.cju.cuhaapi;
+
+import com.cju.cuhaapi.controller.dto.MemberDto;
+import com.cju.cuhaapi.controller.dto.MemberDto.JoinRequest;
+import com.cju.cuhaapi.entity.member.Department;
+import com.cju.cuhaapi.entity.member.Member;
+import com.cju.cuhaapi.entity.member.QMember;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+import static com.cju.cuhaapi.entity.member.Department.DIGITAL_SECURITY;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional
+public class QueryDslTest {
+
+    @Autowired
+    private EntityManager em;
+
+    private JPAQueryFactory queryFactory;
+
+    @BeforeEach
+    void before() {
+        queryFactory = new JPAQueryFactory(em);
+
+        JoinRequest request = JoinRequest.builder()
+                .username("memberA")
+                .password("memberA")
+                .name("홍길동")
+                .isMale(true)
+                .email("test@test.com")
+                .phoneNumber("010-1234-5678")
+                .studentId("0000")
+                .department(DIGITAL_SECURITY)
+                .build();
+
+        Member member = Member.join(request);
+        em.persist(member);
+    }
+
+    @Test
+    void querydsl() {
+        QMember m = QMember.member;
+
+        Member memberA = queryFactory
+                .select(m)
+                .from(m)
+                .where(m.username.eq("memberA"))
+                .fetchOne();
+
+        assertThat(memberA.getUsername()).isEqualTo("memberA");
+        assertThat(memberA.getName()).isEqualTo("홍길동");
+    }
+}
