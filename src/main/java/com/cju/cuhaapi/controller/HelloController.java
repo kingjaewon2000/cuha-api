8a082973014dc4be2be6f52814f7133d2d8e20a5
kingjaewon2000 <kkkxx_00@naver.com>
2022-04-19 04:57:03 +0900

    2022-04-19 (화) rest docs 적용

---
+++ src/main/java/com/cju/cuhaapi/controller/HelloController.java
@@ -0,0 +1,21 @@
+package com.cju.cuhaapi.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public Hello hello() {
+        return new Hello("hi");
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Hello {
+        String content;
+    }
+}
